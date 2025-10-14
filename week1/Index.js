322cd1ea5cdf197955d1b8d67fae1af00c505c4f
Bikash Gharti <bikashghartimagar21@gmail.com>
2025-10-14 02:31:48 +0100

    Update Index.js

--- week1/Index.js
+++ week1/Index.js
@@ -1,1 +1,34 @@
-Index.Js
+const number1 = parseInt(prompt('Enter  your first number : '));
+const number2 = parseInt(prompt('Enter your  second number : '));
+const operator = prompt('Choose your operator("+","-","*","/") : ');
+
+if (operator=="+"){
+
+    result = number1+number2
+    Console.log(result)
+}
+
+else if (operator=="-"){
+
+    result = number1-number2
+    Console.log(result)
+}
+
+else if (operator=="*"){
+
+    result = number1*number2
+    Console.log(result)
+}
+
+else if (operator=="/"){
+
+    result = number1/number2
+    Console.log(result)
+}
+
+else {
+
+    result = prompt("Please enter valid input!")
+    Console.log(result)
+}
+
