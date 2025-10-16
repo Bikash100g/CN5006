cbab66056a78f2db9697034ff79d9b3eabd3aeb4
Bikash Gharti <bikashghartimagar21@gmail.com>
2025-10-16 16:31:30 +0000

    klk

--- week3/index.js
+++ week3/index.js
@@ -15,3 +15,24 @@ var salary = parseInt(prompt("Enter the salary: "))
 //callin the function
 Employeeinfo(name,salary)
 
+const myskills = (skill)=>{
+    console.log("i am expert in "+skill+" programming ")
+
+}
+
+myskills(prompt("Enter your skill: "))
+
+
+const student= require('./StudentInfo')
+const person = require('./Person') 
+// because getName is the function so we use ()
+console.log("Student Name:" +student.getName())
+console.log(student.Location())
+console.log(student.dob)
+// because dob is a variable so we do nt use ()
+console.log(student.Studentgrade())
+console.log("grade is "+student.Studentgrade(55) )
+// creating new Person
+person1= new person("Jim","USA","myemail@gmail.com")
+console.log("using Person Module",person1.getPersonInfo())
+console.log("Programe ended")
