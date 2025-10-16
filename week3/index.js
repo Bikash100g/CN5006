e13737a04544ab3bcabe8f382c239a49f660e879
Bikash Gharti <bikashghartimagar21@gmail.com>
2025-10-16 15:57:05 +0000

    week3

--- week3/index.js
+++ week3/index.js
@@ -1,1 +1,17 @@
+//defining a function
+
+prompt=require('prompt-sync')()
+function Employeeinfo(name,salary){
+
+    console.log("welcome "+name+" your salary is "+salary)
+
+}
+
+console.log("Welcome to the program of the day")
+//declaring variables
+var name = prompt("Enter your name: ")
+var salary = parseInt(prompt("Enter the salary: "))
+
+//callin the function
+Employeeinfo(name,salary)
 
